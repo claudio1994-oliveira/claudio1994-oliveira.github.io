6dc82c4c055048d9fe9ba88489338b323ceebc8b
Cláudio Oliveira <franciscoclaudiooliveira@gmail.com>
2023-11-15 21:32:56 -0300

    criando primeira ideia de página inicial

--- js/main.js
+++ js/main.js
@@ -1,5 +1,11 @@
 const App = {
-
+    data() {
+        return {
+            bio: false,
+        }
+    },
 }; 
 
 Vue.createApp(App).mount('#container');
+
+console.log('Hello World from main.js!');
